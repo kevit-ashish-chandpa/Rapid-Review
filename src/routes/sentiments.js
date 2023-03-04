1d23da2ca5a3b19afac442b8778e24aa70c477d4
kevit-ashish-chandpa <ashish.chandpa@kevit.io>
2023-03-04 20:21:27 +0530

    Sentiments added in the codes.

--- src/routes/sentiments.js
+++ src/routes/sentiments.js
@@ -1,13 +1,37 @@
 const express = require("express");
 const router = express.Router();
-
+const { Configuration, OpenAIApi, CreateCompletionRequest } = require('openai');
 // const sharp = require("sharp");
 // const { sendWelcomeEmail, sendCancelEmail } = require("../emails/account");
 
-router.get("/sentiments", async (req,res)=>{
+router.post("/sentiments", async (req, res) => {
     try {
-        console.log(req.body)
-        res.send(req.body)
+        let comment = "It is a ber good product!"
+        const configuration = new Configuration({
+            apiKey: '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'//commonConfigurations.openAIAPIKey,
+        });
+        const openAIApi = new OpenAIApi(configuration)
+
+        const params = {
+            prompt: `Decide whether a product comment sentiment is positive, neutral, or negative give me this words only: ${comment}`,
+            model: 'text-davinci-003',
+            temperature: 0.9,
+            max_tokens: 64,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+        }
+
+        const { data } = await openAIApi.createCompletion(params)
+
+        const sentiment = {
+            sentiment: data.choices[0].text
+                .match(/\b(negative|positive|neutral)\b/i)[0]
+                .trim()
+                .toUpperCase()
+        };
+        console.log(sentiment, 'response before sending to client');
+        res.send(sentiment);
     } catch (error) {
         console.log(error)
     }
